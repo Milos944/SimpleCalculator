6527479b1d184eb973f49709d0c0480d226a801c
Milos <milos904@icloud.com>
2020-03-27 19:13:45 +0100

    Initial commit

---
+++ SimpleCalculator.py
@@ -0,0 +1,51 @@
+import math
+#Creating two variables that will hold two random numbers:
+
+#Output adding
+num1 = input("Pick a number:")
+num2 = input("Pick another number:")
+result0 = float(num1) + float(num2)
+print("Value is:")
+print(result0)
+
+#Output subtracting
+num3 = input("Pick a number:")
+num4 = input("Pick another number:")
+result2 = float(num3) - float(num4)
+print("Value is:")
+print(result2)
+
+#Output multiple
+num5 = input("Pick a number:")
+num6 = input("Pick another number")
+result3 = float(num5) * float(num6)
+print("Value is:")
+print(result3)
+
+#Output divide
+num7 = input("Pick a number:")
+num8 = input("Pick another number")
+result4 = float(num7) / float(num8)
+print("Value is:")
+print(result4)
+
+#Output module
+num9 = input("Pick a number:")
+num10 = input("Pick another number")
+result5 = float(num9) % float(num10)
+print("Value is:")
+print(result5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
